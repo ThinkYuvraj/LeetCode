779232e726c47c064f94176bb57ae1c52e2880d0
Yuvraj Singh <thinkyuvraj@gmail.com>
2026-07-31 02:59:04 +0530

    Time: 0 ms (100%), Space: 53.9 MB (63.33%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+
+var minimumPushes = function (word) {
+    let pushes = 0;
+    for (let i = 0; i < word.length; i++) {
+        pushes += Math.floor(i / 8) + 1;
+    }
+    return pushes;
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
