3587849e56e7ca103b2b14944ea795873ce6d4bb
Yuvraj Singh <thinkyuvraj@gmail.com>
2026-08-12 02:19:44 +0530

    Time: 2 ms (21.28%), Space: 55 MB (76.6%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingInteger = function(nums) {
+    let sum = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1] + 1) {
+            sum += nums[i];
+        } else {
+            break;
+        }
+    }
+
+    const set = new Set(nums);
+
+    while (set.has(sum)) {
+        sum++;
+    }
+
+    return sum;
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
