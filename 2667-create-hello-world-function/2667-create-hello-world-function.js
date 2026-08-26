fd8b6fa60bc1d3c67c5036fcf6140197544519b5
Yuvraj Singh <thinkyuvraj@gmail.com>
2026-08-27 01:08:50 +0530

    Time: 45 ms (39.55%), Space: 53.9 MB (24.38%) - LeetHub

--- 2667-create-hello-world-function/2667-create-hello-world-function.js
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -12,7 +12,3 @@ var createHelloWorld = function() {
  * const f = createHelloWorld();
  * f(); // "Hello World"
  */
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
